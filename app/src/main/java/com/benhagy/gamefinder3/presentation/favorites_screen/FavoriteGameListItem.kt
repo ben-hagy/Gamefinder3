aad0ab2330bc5c21e66735356a64fcc95ddc397d
Ben Hagy <benhagy@alumni.vcu.edu>
2023-05-18 14:52:40 -0400

    removing games from the details screen now works; improved details UI a bit

--- app/src/main/java/com/benhagy/gamefinder3/presentation/favorites_screen/FavoriteGameListItem.kt
+++ app/src/main/java/com/benhagy/gamefinder3/presentation/favorites_screen/FavoriteGameListItem.kt
@@ -1,5 +1,6 @@
 package com.benhagy.gamefinder3.presentation.favorites_screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,11 +10,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -28,42 +35,54 @@ fun FavoriteGameListItem(
     modifier: Modifier = Modifier
 ) {
     Row(
-        modifier = modifier,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(4.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Column(
+//        Column(
+//            modifier = modifier
+//                .clipToBounds()
+//                .padding(4.dp),
+//            verticalArrangement = Arrangement.Center,
+//            horizontalAlignment = Alignment.CenterHorizontally
+//
+//        ) {
+//            AsyncImage(
+//                model = game.backgroundImage,
+//                contentDescription = null,
+//                modifier = modifier
+//                    .height(60.dp)
+//                    .width(60.dp)
+//                    .clipToBounds()
+//                    .clip(shape = RoundedCornerShape(50)),
+//                contentScale = ContentScale.Crop,
+//                alignment = Alignment.TopStart
+//            )
+//        }
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(
+            text = game.name.toString(),
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 16.sp,
+            color = MaterialTheme.colorScheme.onBackground,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
             modifier = Modifier.weight(1f)
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                AsyncImage(
-                    model = game.backgroundImage,
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop,
-                    modifier = modifier
-                        .fillMaxHeight()
-                        .size(80.dp)
-                        .padding(2.dp)
-
-                )
-                Text(
-                    text = game.name.toString(),
-                    fontWeight = FontWeight.SemiBold,
-                    fontSize = 16.sp,
-                    color = MaterialTheme.colorScheme.onBackground,
-                    overflow = TextOverflow.Ellipsis,
-                    maxLines = 1,
-                    modifier = Modifier.weight(1f)
-                )
-                Spacer(modifier = Modifier.width(4.dp))
-                Text(
-                    text = game.released.toString(),
-                    fontWeight = FontWeight.Light,
-                    color = MaterialTheme.colorScheme.onBackground
-                )
-            }
-            Spacer(modifier = Modifier.height(8.dp))
-        }
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(
+            text = game.released.toString(),
+            fontWeight = FontWeight.Light,
+            color = MaterialTheme.colorScheme.onBackground
+        )
+        Icon(
+            imageVector = Icons.Default.Delete,
+            contentDescription = "Delete favorite item",
+            modifier = modifier
+                .padding(4.dp)
+        )
     }
+    Spacer(modifier = Modifier.height(8.dp))
 }
+
--- app/src/main/java/com/benhagy/gamefinder3/presentation/game_details_screen/GameDetailsScreen.kt
+++ app/src/main/java/com/benhagy/gamefinder3/presentation/game_details_screen/GameDetailsScreen.kt
@@ -2,6 +2,7 @@ package com.benhagy.gamefinder3.presentation.game_details_screen
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,11 +11,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Schedule
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -25,6 +30,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -54,66 +60,126 @@ fun GameDetailsScreen(
     val pagerState = rememberPagerState()
     val scroll = rememberScrollState(0)
     val coroutineScope = rememberCoroutineScope()
-    if (state.error == null) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp)
-        ) {
-            Row(
+    state.gameDetails?.let { gameDetails ->
+        if (state.error == null) {
+            Column(
                 modifier = Modifier
-                    .background(color = MaterialTheme.colorScheme.background)
-                    .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
+                    .fillMaxSize()
+                    .padding(8.dp)
             ) {
-                IconButton(
-                    onClick = {
-                        navigator.popBackStack()
-                    },
+                Row(
+                    modifier = Modifier
+                        .background(color = MaterialTheme.colorScheme.background)
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_back_arrow),
-                        contentDescription = "Go back",
-                        tint = MaterialTheme.colorScheme.onBackground,
-                        modifier = Modifier.size(28.dp)
-                    )
+                    IconButton(
+                        onClick = {
+                            navigator.popBackStack()
+                        },
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_back_arrow),
+                            contentDescription = "Go back",
+                            tint = MaterialTheme.colorScheme.onBackground,
+                            modifier = Modifier.size(28.dp)
+                        )
+                    }
+                    IconButton(
+                        onClick = {
+                            coroutineScope.launch {
+                                if(state.isFavorite) {
+                                    viewModel.onEvent(
+                                        GameDetailsScreenEvent.RemoveGameFromFavorites(id = state.gameDetails.id!!)
+                                    // snackbar with undo button?
+                                    )
+                                } else {
+                                    viewModel.onEvent(
+                                        GameDetailsScreenEvent.SaveGameAsFavorite(game = state.gameDetails)
+                                    )
+                                }
+                            }
+                        },
+                        modifier = Modifier
+                            .padding(4.dp)
+                    ) {
+                        Icon(
+                            painter = if (state.isFavorite) {
+                                painterResource(id = R.drawable.ic_favorite_filled)
+                            } else {
+                                painterResource(id = R.drawable.ic_favorite_unfilled)
+                            },
+                            contentDescription = "Add to favorites",
+                            tint = MaterialTheme.colorScheme.onBackground,
+                            modifier = Modifier.size(28.dp)
+                        )
+
+                    }
                 }
-                state.gameDetails?.let { gameDetails ->
-                    Text(
-                        text = gameDetails.name.toString(),
-                        fontFamily = montserratFonts,
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.SemiBold,
-                        textAlign = TextAlign.Center
-                    )
-                }
-                IconButton(
-                    onClick = {
-                        coroutineScope.launch {
-                            viewModel.onEvent(
-                                GameDetailsScreenEvent.SaveGameAsFavorite(game = state.gameDetails!!)
+                Row {
+                    AsyncImage(
+                        model = gameDetails.backgroundImage ?: "",
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        alignment = Alignment.TopCenter,
+                        modifier = Modifier
+                            .padding(horizontal = 8.dp)
+                            .size(80.dp)
+                            .clipToBounds()
+                            .border(
+                                width = 2.dp,
+                                color = MaterialTheme.colorScheme.secondary,
+                                shape = CircleShape
                             )
+                            .clip(CircleShape)
+                    )
+                    Column {
+                        Text(
+                            modifier = Modifier.padding(
+                                vertical = 4.dp,
+                                horizontal = 4.dp
+                            ),
+                            text = gameDetails.name.toString(),
+                            fontFamily = montserratFonts,
+                            fontSize = 28.sp,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                        Row(
+                            modifier = Modifier
+                                .padding(horizontal = 16.dp)
+                        ) {
+                            Row {
+                                Icon(
+                                    modifier = Modifier.size(16.dp),
+                                    imageVector = Icons.Rounded.Schedule,
+                                    contentDescription = "Released",
+                                )
+                                Text(
+                                    modifier = Modifier.padding(
+                                        horizontal = 4.dp
+                                    ),
+                                    text = gameDetails.released ?: "Unknown",
+                                    style = MaterialTheme.typography.bodySmall.copy(
+                                        color = MaterialTheme.colorScheme.onTertiaryContainer,
+                                        fontWeight = FontWeight.Normal
+                                    ),
+                                )
+                            }
+
+                            Spacer(Modifier.width(32.dp))
+
                         }
                     }
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_favorite_unfilled),
-                        contentDescription = "Add to favorites",
-                        tint = MaterialTheme.colorScheme.onBackground,
-                        modifier = Modifier.size(28.dp)
-                    )
 
                 }
-            }
-            Spacer(modifier = Modifier.height(2.dp))
-            Divider(
-                thickness = 1.dp,
-                color = MaterialTheme.colorScheme.onBackground
-            )
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                state.gameDetails?.let { gameDetails ->
+                Spacer(modifier = Modifier.height(2.dp))
+                Divider(
+                    thickness = 1.dp,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
                     HorizontalPager(
                         pageCount = gameDetails.screenshots.size,
                         state = pagerState,
@@ -133,12 +199,10 @@ fun GameDetailsScreen(
                         )
                     }
                 }
-            }
-            Spacer(modifier = Modifier.height(8.dp))
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                state.gameDetails?.let { gameDetails ->
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
                     Text(
                         text = parse(gameDetails.description.toString()),
                         fontFamily = montserratFonts,
