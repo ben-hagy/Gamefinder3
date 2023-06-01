fb87954f3cbae0d6d6878a787562e88883986e76
Ben Hagy <benhagy@alumni.vcu.edu>
2023-06-01 13:05:46 -0400

    safety commit before i start messing with some stuff

--- app/src/main/java/com/benhagy/gamefinder3/presentation/bookmarks_screen/components/TransparentHintTextField.kt
+++ app/src/main/java/com/benhagy/gamefinder3/presentation/bookmarks_screen/components/TransparentHintTextField.kt
@@ -3,11 +3,14 @@ package com.benhagy.gamefinder3.presentation.bookmarks_screen.components
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import com.benhagy.gamefinder3.presentation.ui.theme.montserratFonts
@@ -22,8 +25,12 @@ fun TransparentHintTextField(
 ) {
     Box(
         modifier = modifier
+            .shadow(
+                elevation = 0.2.dp,
+                shape = RoundedCornerShape(30)
+            )
     ) {
-        OutlinedTextField(
+        TextField(
             value = text,
             onValueChange = onValueChange,
             singleLine = singleLine,
